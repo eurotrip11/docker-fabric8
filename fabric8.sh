da4366642e7630c233b049c684032b42e90515de
Giuseppe Trisciuoglio <giuseppe.trisciuoglio@gmail.com>
2015-06-14 15:12:14 +0200

    fix vari

--- fabric8.sh
+++ fabric8.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Starting the fabric8 container"
-docker run -p 8101:8101 -p 8181:8181 -d -t --name fabric8demo -e DOCKER_HOST=http://192.168.59.103:2375 eurotrip:fabric8
+docker pull sonatype/nexus:oss
+docker run -d -p 8081:8081 --name nexus sonatype/nexus:oss
+docker run -p 8101:8101 -p 8181:8181 -d -t --link nexus:nexus --name fabric8 -e DOCKER_HOST=http://192.168.59.103:2375 eurotrip:fabric8
 #docker port fabric8demo 8181
 #docker port fabric8demo 8101
